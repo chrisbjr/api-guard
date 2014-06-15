82de08d1d1a2db5f7398fb24ea04bbe2fff87850
Chris Bautista <chrisbjr@gmail.com>
2014-06-15 11:50:40 +0800

    Created a config file to manage some settings

---
+++ src/config/config.php
@@ -0,0 +1,29 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | API request logging
+    |--------------------------------------------------------------------------
+    |
+    | This switch will enable or disable logging of the requests made. This
+    | feature needs to be enabled for API request limiting to work
+    |
+    */
+
+    'logging' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Key name
+    |--------------------------------------------------------------------------
+    |
+    | This is the name of the variable that will provide us the API key in the
+    | header
+    |
+    */
+
+    'keyName' => 'X-API-KEY',
+
+);
