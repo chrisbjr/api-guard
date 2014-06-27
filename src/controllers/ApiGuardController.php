88546b53ff435a04e510bc83a6938a31c9dfc0a3
Chris Bautista <chrisbjr@gmail.com>
2014-06-27 12:48:37 +0800

    Re-implemented old response method for older APIs to use

--- src/controllers/ApiGuardController.php
+++ src/controllers/ApiGuardController.php
@@ -110,4 +110,56 @@ class ApiGuardController extends Controller
         }, ['apiMethods' => $this->apiMethods]);
     }
 
+    public function response($data = null, $httpStatusCode = 200, $error = null)
+    {
+        $status = [
+            200 => 'OK',
+            201 => 'Created',
+            202 => 'Accepted',
+            203 => 'Non-Authoritative Information',
+            204 => 'No Content',
+            205 => 'Reset Content',
+            206 => 'Partial Content',
+            300 => 'Multiple Choices',
+            301 => 'Moved Permanently',
+            302 => 'Found',
+            304 => 'Not Modified',
+            305 => 'Use Proxy',
+            307 => 'Temporary Redirect',
+            400 => 'Bad Request',
+            401 => 'Unauthorized',
+            403 => 'Forbidden',
+            404 => 'Not Found',
+            405 => 'Method Not Allowed',
+            406 => 'Not Acceptable',
+            407 => 'Proxy Authentication Required',
+            408 => 'Request Timeout',
+            409 => 'Conflict',
+            410 => 'Gone',
+            411 => 'Length Required',
+            412 => 'Precondition Failed',
+            413 => 'Request Entity Too Large',
+            414 => 'Request-URI Too Long',
+            415 => 'Unsupported Media Type',
+            416 => 'Requested Range Not Satisfiable',
+            417 => 'Expectation Failed',
+            500 => 'Internal Server Error',
+            501 => 'Not Implemented',
+            502 => 'Bad Gateway',
+            503 => 'Service Unavailable',
+            504 => 'Gateway Timeout',
+            505 => 'HTTP Version Not Supported'
+        ];
+
+        $output['code'] = $httpStatusCode;
+        $output['status'] = $status[$httpStatusCode];
+        if ($error !== null) $output['error'] = $error;
+
+        if ($data != null) {
+            $output['data'] = $data;
+        }
+
+        return \Illuminate\Support\Facades\Response::make($output, $httpStatusCode, ['Content-type' => 'application/json']);
+    }
+
 }
