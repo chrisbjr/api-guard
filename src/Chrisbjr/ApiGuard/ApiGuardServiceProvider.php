14712295f19fb0123d560f83edc88112c0c72214
Chris Bautista <chrisbjr@gmail.com>
2014-12-21 21:01:01 +0800

    Formatting changes

--- src/Chrisbjr/ApiGuard/ApiGuardServiceProvider.php
+++ src/Chrisbjr/ApiGuard/ApiGuardServiceProvider.php
@@ -2,47 +2,48 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class ApiGuardServiceProvider extends ServiceProvider {
+class ApiGuardServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('chrisbjr/api-guard');
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('chrisbjr/api-guard');
 
         $this->app->register('EllipseSynergie\ApiResponse\Laravel\ResponseServiceProvider');
 
-        require_once __DIR__.'/../../routes.php';
-	}
+        require_once __DIR__ . '/../../routes.php';
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 
 }
