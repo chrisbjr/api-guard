d107eb8f3bd24aeba698a710f93308c0fdd600fd
John Connor <John Connor>
2016-01-13 19:40:18 +0100

    Fixed API key access rate

--- src/Http/Middleware/ApiGuard.php
+++ src/Http/Middleware/ApiGuard.php
@@ -101,6 +101,62 @@ class ApiGuard
 
       $apiLog = App::make(Config::get('apiguard.apiLogModel', 'Chrisbjr\ApiGuard\Models\ApiLog'));
 
+      // Then check the limits of this method
+      if (!empty($apiMethods[$method]['limits'])) {
+          if (Config::get('apiguard.logging', true) === false) {
+              Log::warning("[ApiGuard] You specified a limit in the $method method but API logging needs to be enabled in the configuration for this to work.");
+          }
+          $limits = $apiMethods[$method]['limits'];
+          // We get key level limits first
+          if ($this->apiKey != null && ! empty($limits['key'])) {
+              $keyLimit = ( ! empty($limits['key']['limit'])) ? $limits['key']['limit'] : 0;
+              if ($keyLimit == 0 || is_integer($keyLimit) == false) {
+                  Log::warning("[ApiGuard] You defined a key limit to the " . Route::currentRouteAction() . " route but you did not set a valid number for the limit variable.");
+              } else {
+                  if ( ! $this->apiKey->ignore_limits) {
+                      // This means the apikey is not ignoring the limits
+                      $keyIncrement = ( ! empty($limits['key']['increment'])) ? $limits['key']['increment'] : Config::get('apiguard.keyLimitIncrement', '1 hour');
+                      $keyIncrementTime = strtotime('-' . $keyIncrement);
+                      if ($keyIncrementTime == false) {
+                          Log::warning("[ApiGuard] You have specified an invalid key increment time. This value can be any value accepted by PHP's strtotime() method");
+                      } else {
+                          // Count the number of requests for this method using this api key
+                          $apiLogCount = $apiLog->countApiKeyRequests($this->apiKey->id, Route::currentRouteAction(), $request->getMethod(), $keyIncrementTime);
+                          if ($apiLogCount >= $keyLimit) {
+                              Log::warning("[ApiGuard] The API key ID#{$this->apiKey->id} has reached the limit of {$keyLimit} in the following route: " . Route::currentRouteAction());
+                              return $response->setStatusCode(429)->withError('You have reached the limit for using this API.', 'GEN-TOO-MANY-REQUESTS');
+                          }
+                      }
+                  }
+              }
+          }
+
+          // Then the overall method limits
+          if ( ! empty($limits['method'])) {
+              $methodLimit = ( ! empty($limits['method']['limit'])) ? $limits['method']['limit'] : 0;
+              if ($methodLimit == 0 || is_integer($methodLimit) == false) {
+                  Log::warning("[ApiGuard] You defined a method limit to the " . Route::currentRouteAction() . " route but you did not set a valid number for the limit variable.");
+              } else {
+                  if ($this->apiKey != null && $this->apiKey->ignore_limits == true) {
+                      // then we skip this
+                  } else {
+                      $methodIncrement = ( ! empty($limits['method']['increment'])) ? $limits['method']['increment'] : Config::get('apiguard.keyLimitIncrement', '1 hour');
+                      $methodIncrementTime = strtotime('-' . $methodIncrement);
+                      if ($methodIncrementTime == false) {
+                          Log::warning("[ApiGuard] You have specified an invalid method increment time. This value can be any value accepted by PHP's strtotime() method");
+                      } else {
+                          // Count the number of requests for this method
+                          $apiLogCount = $apiLog->countMethodRequests(Route::currentRouteAction(), $request->getMethod(), $methodIncrementTime);
+                          if ($apiLogCount >= $methodLimit) {
+                              Log::warning("[ApiGuard] The API has reached the method limit of {$methodLimit} in the following route: " . Route::currentRouteAction());
+                              return $response->setStatusCode(429)->withError('The limit for using this API method has been reached', 'GEN-TOO-MANY-REQUESTS');
+                          }
+                      }
+                  }
+              }
+          }
+      }
+
       // End of cheking limits
       if (Config::get('apiguard.logging', true)) {
           // Default to log requests from this action
