0c23a73962e539d2440d310d64ce9c4adc745aff
Debiprasad Sahoo <debiprasad.s@gmail.com>
2016-06-03 20:28:11 +0530

    Added feature to control default behaviour whether to do key auth for methods via settings.

--- src/Chrisbjr/ApiGuard/Controllers/ApiGuardController.php
+++ src/Chrisbjr/ApiGuard/Controllers/ApiGuardController.php
@@ -69,7 +69,11 @@ class ApiGuardController extends Controller
             $method = last($routeArray);
 
             // We should check if key authentication is enabled for this method
-            $keyAuthentication = true;
+            if (Config::get('api-guard::keyAuthentication')) {
+                $keyAuthentication = Config::get('api-guard::keyAuthentication');
+            } else {
+                $keyAuthentication = true;
+            }
             if (isset($apiMethods[$method]['keyAuthentication']) && $apiMethods[$method]['keyAuthentication'] === false) {
                 $keyAuthentication = false;
             }
